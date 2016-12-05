d7ab88ee7c2bd7cca6d78e39c45eb1fbc9b84839
Murray Edmunds <murray.edmunds@live.com>
2016-12-04 20:39:21 -0700

    added custom validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Validator::extend('email_exists', function ($attribute, $value, $parameters, $validator) {
+            $email = \App\Users::whereEmail($_POST['email'])->first();
+            if ($email) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        \Validator::extend('valid_password', function ($attribute, $value, $parameters, $validator) {
+            $user = \App\Users::whereEmail($_POST['email'])->first();
+            $userPassword = $user['password'];
+            return password_verify($_POST['password'], $userPassword);
+        });
     }
 
     /**
