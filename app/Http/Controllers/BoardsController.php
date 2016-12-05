3a59bb8ef80d6743e66f33b02d16b763e6ccf2b9
Murray Edmunds <murray.edmunds@live.com>
2016-12-04 20:38:46 -0700

    added function to load main page added function to logout session

--- app/Http/Controllers/BoardsController.php
+++ app/Http/Controllers/BoardsController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class BoardsController extends Controller
 {
-    //
+    public function loadMain()
+    {
+        if (session('isLoggedIn') == false) {
+            return redirect('/');
+        } else {
+            return view('home', [
+                'boards' => \App\Boards::whereUserId(session('id'))->get()
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->session()->flush();
+        return redirect('/');
+    }
 }
