aedd9a88adcc43ef429cf4258137563ef3844a9f
Murray Edmunds <murray.edmunds@live.com>
2016-12-04 21:04:52 -0700

    added function to save board id's

--- app/Http/Controllers/BoardsController.php
+++ app/Http/Controllers/BoardsController.php
@@ -18,6 +18,31 @@ class BoardsController extends Controller
         }
     }
 
+    public function saveBoard()
+    {
+        $validator = \Validator::make($_POST, [
+            'board_name' => 'required',
+            'board_id' => 'required'
+        ], [
+            'board_name.required' => 'Please enter a name for the board.',
+            'board_id.required' => 'Please enter a board ID.'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/assignment3/main.php')
+                ->withErrors($validator)
+                ->withInput();
+        } else {
+            $boards = new \App\Boards;
+            $boards->user_id = session('id');
+            $boards->name = $_POST['board_name'];
+            $boards->board_id = $_POST['board_id'];
+            $boards->save();
+
+            return redirect('/home/');
+        }
+    }
+
     public function logout(Request $request)
     {
         $request->session()->flush();
