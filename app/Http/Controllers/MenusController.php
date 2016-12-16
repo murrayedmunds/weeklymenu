a11602accb23c93fd1971b0c1a37b229895cfc2f
Murray Edmunds <murray.edmunds@live.com>
2016-12-16 14:02:34 -0700

    add function loadMenu added from saveMenu from BoardsController

--- app/Http/Controllers/MenusController.php
+++ app/Http/Controllers/MenusController.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class MenusController extends Controller
 {
-    //
+
+    public function loadMenus()
+    {
+        if (session('isLoggedIn') == false) {
+            return redirect('/');
+        } else {
+            return view('menus', [
+                'menus' => \App\Menus::whereUserId(session('user_id'))->get()
+            ]);
+        }
+    }
+
+    public function saveMenu()
+    {
+        $validator = \Validator::make($_POST, [
+            'menuName' => 'required',
+        ], [
+            'menuName.required' => 'Please enter a name for the menu.',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/home/')
+                ->with('form', 'menuError')
+                ->withErrors($validator)
+                ->withInput();
+        } else {
+            $menus = new \App\Menus;
+            $menus->user_id = session('user_id');
+            $menus->name = $_POST['menuName'];
+            $menus->mainMondayUrl = $_POST['mainMondayUrl'];
+            $menus->mainMondayMetadataLinkTitle = $_POST('mainMondayMetadataLinkTitle');
+            $menus->mainMondayMetadataLinkDescription = $_SESSION('mainMondayMetadataLinkDescription');
+            $menus->mainMondayNote = $_SESSION('mainMondayNote');
+            $menus->mainMondayImageUrl = $_SESSION('mainMondayImageUrl');
+            $menus->save();
+
+            return redirect('/menus/');
+        }
+    }
 }
